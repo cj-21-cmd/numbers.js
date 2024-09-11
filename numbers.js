f21ad065721cfd11b86b7c945b7e8269f422e2fd
cjdelacruz <cjdelacruz0521@gmail.com>
2024-09-11 22:32:19 +0700

    Initial commit with numbers.js

---
+++ numbers.js
@@ -0,0 +1,20 @@
+const firstFavNum = 21;
+const secondFavNum = 5;
+
+const sum = firstFavNum + secondFavNum;
+console.log("addition:", sum);
+
+const sub = firstFavNum - secondFavNum;
+console.log("subraction:", sub);
+
+const product = firstFavNum * secondFavNum;
+console.log("multiplication:", product);
+
+const quotient = firstFavNum / secondFavNum;
+console.log("division:", quotient);
+
+const modulos = firstFavNum % secondFavNum;
+console.log("modulos:", modulos);
+
+const exponent = firstFavNum ** secondFavNum;
+console.log("exponentiation:", exponent);
